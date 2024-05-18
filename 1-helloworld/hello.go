8545b31c19ab537d5564a38d2902d8c94d1602a7
viveksingh-01 <viveksingh9301@gmail.com>
2024-05-18 08:39:14 +0530

    Add default greeting when user-name is not available

--- 1-helloworld/hello.go
+++ 1-helloworld/hello.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func Hello(name string) string {
+	if name == "" {
+		return "Hello, world"
+	}
 	return "Hello, " + name
 }
 
 func main() {
-	user := "Robert";
+	user := "Mike";
 	fmt.Print(Hello(user))
 }
--- 1-helloworld/hello_test.go
+++ 1-helloworld/hello_test.go
@@ -3,9 +3,18 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris");
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+	t.Run("saying to hello people", func(t *testing.T) {
+		got := Hello("Chris");
+		want := "Hello, Chris"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}	
+	})
+	t.Run("empty string defaults to 'Hello, world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
