721f38ca4fc5f7a5171fa93a0ced3ea113daee0b
viveksingh-01 <viveksingh9301@gmail.com>
2024-05-18 12:31:15 +0530

    Refactor assertion test logic in TestHello function

--- 1-helloworld/hello_test.go
+++ 1-helloworld/hello_test.go
@@ -6,15 +6,20 @@ func TestHello(t *testing.T) {
 	t.Run("saying to hello people", func(t *testing.T) {
 		got := Hello("Chris");
 		want := "Hello, Chris"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}	
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("empty string defaults to 'Hello, world'", func(t *testing.T) {
 		got := Hello("")
-		want := "Hello, world"
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		want := "Hello, worl"
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// t.Helper() is needed to tell the test suite that this method is a helper. By doing this, when it fails, 
+	// the line number reported will be in our function call rather than inside our test helper. 
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
