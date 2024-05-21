0378c804c68ed253f13cb7eefc4b89007de21dec
viveksingh-01 <viveksingh9301@gmail.com>
2024-05-21 09:10:22 +0530

    Iterations in Go

---
+++ 2-iterations/repeat_test.go
@@ -0,0 +1,11 @@
+package iterations
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	got := Repeat("a")
+	expected := "aaaaa"
+	if got != expected {
+		t.Errorf("expected %q but got %q", expected, got)
+	}
+}
