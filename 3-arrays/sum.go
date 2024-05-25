26db1f358f627e11d6df4c0dc395bc838b1f5bd7
viveksingh-01 <viveksingh9301@gmail.com>
2024-05-25 19:10:20 +0530

    Refactor iteration logic in Sum by using 'range'

--- 3-arrays/sum.go
+++ 3-arrays/sum.go
@@ -2,8 +2,11 @@ package arrays
 
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+	// "range" lets you iterate over an array.
+	// On each iteration, range returns two values - the index and the value.
+	// We are choosing to ignore the index value by using "_" blank identifier.
+	for _, num := range numbers {
+		sum += num
 	}
 	return sum
 }
