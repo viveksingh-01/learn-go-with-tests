fcdc3c4ebd37aa6aed46b2e46fdb074b1d1a6c6d
viveksingh-01 <viveksingh9301@gmail.com>
2024-05-26 08:38:37 +0530

    Slices in Go

--- 3-arrays/sum.go
+++ 3-arrays/sum.go
@@ -1,10 +1,17 @@
 package arrays
 
-func Sum(numbers [5]int) int {
+func SumOfArray(numbers [5]int) int {
 	sum := 0
 	// "range" lets you iterate over an array.
 	// On each iteration, range returns two values - the index and the value.
 	// We are choosing to ignore the index value by using "_" blank identifier.
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
+}
+
+func SumOfSlice(numbers []int) (sum int) {
 	for _, num := range numbers {
 		sum += num
 	}
--- 3-arrays/sum_test.go
+++ 3-arrays/sum_test.go
@@ -3,16 +3,34 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	// Arrays have a fixed capacity which you define when you declare the variable. 
-	// We can initialize an array in two ways:
-	// [N]type{value1, value2, ..., valueN} e.g. numbers := [5]int{1, 2, 3, 4, 5}
-	// [...]type{value1, value2, ..., valueN} e.g. numbers := [...]int{1, 2, 3, 4, 5}
-	numbers := [5]int{1, 2, 3, 4,5 }
+	t.Run("sum of collection of 5 numbers", func(t *testing.T) {
+		// Arrays have a fixed capacity which you define when you declare the variable. 
+		// We can initialize an array in two ways:
+		// [N]type{value1, value2, ..., valueN} e.g. numbers := [5]int{1, 2, 3, 4, 5}
+		// [...]type{value1, value2, ..., valueN} e.g. numbers := [...]int{1, 2, 3, 4, 5}
+		numArray := [5]int{1, 2, 3, 4,5 }
+		
+		got := SumOfArray(numArray)
+		want := 15
+		if got != want {
+			// It is sometimes useful to also print the inputs to the function in the error message. 
+			// Here, we are using the %v placeholder to print the "default" format, which works well for arrays.
+			t.Errorf("got %d want %d, given %v", got, want, numArray)
+		}
+	})
 
-	got := Sum(numbers)
-	want := 15
-	if got != want {
-		// It is sometimes useful to also print the inputs to the function in the error message. Here, we are using the %v placeholder to print the "default" format, which works well for arrays.
-		t.Errorf("got %d want %d, given %v", got, want, numbers)
-	}
+	// NOTE:
+	// An interesting property of arrays is that the size is encoded in its type. 
+	// If you try to pass an [4]int into a function that expects [5]int, it won't compile. 
+	// They are different types so it's just the same as trying to pass a string into a function that wants an int.
+
+	t.Run("sum of collection of any size", func(t *testing.T) {
+		// Go has "slices" which do not encode the size of the collection and instead can have any size.
+		numSlice := []int{1, 2, 3}
+		got := SumOfSlice(numSlice)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numSlice)
+		}
+	})
 }
