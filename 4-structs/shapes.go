bc205cb316c59b0e355100983ef572fa4b2122c8
viveksingh-01 <viveksingh9301@gmail.com>
2025-01-08 09:43:27 +0530

    Receiver functions in Go
    - add Circle struct and Area method
    - update tests for rectangle and circle areas

--- 4-structs/shapes.go
+++ 4-structs/shapes.go
@@ -1,5 +1,7 @@
 package structs
 
+import "math"
+
 // A struct is just a named collection of fields where you can store data.
 // They are a way to create more complex data types.
 type Rectangle struct {
@@ -13,4 +15,15 @@ func Perimeter(rectangle Rectangle) float64 {
 
 func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+// A method is a function with a special receiver argument.
+// Methods are very similar to functions but they are called by invoking them on an instance of a particular type.
+// Here we are defining the 'Area' method with receiver type 'Circle'.
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- 4-structs/shapes_test.go
+++ 4-structs/shapes_test.go
@@ -12,10 +12,28 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 15.0}
-	got := Area(rectangle)
-	want := 150.0
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := Area(rectangle)
+		want := 72.0
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		// got := Area(circle) --> this will throw error
+		// since we cannot use circle (variable of type Circle) as Rectangle value in argument to Area
+		// also, we can't declare another Area function with Circle as argument.
+		// so, we have only two options:
+		// 1. define another Area function with Circle as argument in a different package
+		// 2. use methods (Receiver functions) to define Area function with Circle as argument.
+		// we will use the second option.
+		got := circle.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %g, want %g", got, want)
+		}
+	})
 }
