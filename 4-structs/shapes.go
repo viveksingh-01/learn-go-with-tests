6c3a7d396f9b2acd2ecf71cdf9687e49da1fa2b9
viveksingh-01 <viveksingh9301@gmail.com>
2025-01-09 22:39:28 +0530

    Interfaces in Go
    Implement Shape interface and checkArea function for area calculations of Rectangle and Circle

--- 4-structs/shapes.go
+++ 4-structs/shapes.go
@@ -9,25 +9,53 @@ type Rectangle struct {
 	Height float64
 }
 
-func (r Rectangle) Perimeter() float64 {
-	return 2 * (r.Width + r.Height)
+type Circle struct {
+	Radius float64
+}
+
+// Instead of defining 2 different methods to calculate Area,
+// we want to be able to write some kind of checkArea function that we can pass 
+// both Rectangles and Circles to, but fail to compile if we try to pass in something that isn't a shape.
+
+// Interfaces are very powerful concept in statically-typed languages like Go.
+// They allow us to make functions that can be used with different types and create highly-decoupled code.
+
+// But how does something become a shape? We just tell Go what a Shape is, using an interface declaration
+// We can do this by defining a Shape interface that defines a method Area() that returns a float64.
+// Any type that defines a method Area() that returns a float64 satisfies the Shape interface.
+type Shape interface {
+	Area() float64
+}
+
+// Now we can define a function checkArea that takes a Shape and returns a float64.
+// We can pass in any type that satisfies the Shape interface, like Rectangle or Circle.
+// If we try to pass in something that doesn't satisfy the Shape interface, the Go compiler will throw an error.
+// This is a great way to make sure our code is correct and decoupled.
+
+
+func checkArea(s Shape) float64 {
+	return s.Area()
 }
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
-type Circle struct {
-	Radius float64
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 func (c Circle) Perimeter() float64 {
 	return 2 * math.Pi * c.Radius
 }
 
-// A method is a function with a special receiver argument.
-// Methods are very similar to functions but they are called by invoking them on an instance of a particular type.
-// Here we are defining the 'Area' method with receiver type 'Circle'.
-func (c Circle) Area() float64 {
-	return math.Pi * c.Radius * c.Radius
-}
+// More on Interfaces:
+// This is a powerful concept in Go and is used throughout the standard library.
+// For example, the fmt package uses interfaces to print different types of data with the same function.
+// This is why fmt.Println can print a string, an integer, a float, etc.
+// The io package uses interfaces to read and write data from different sources like files, network connections, and in-memory data.
+// 
--- 4-structs/shapes_test.go
+++ 4-structs/shapes_test.go
@@ -14,7 +14,7 @@ func TestPerimeter(t *testing.T) {
 func TestArea(t *testing.T) {
 	t.Run("rectangle", func(t *testing.T) {
 		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
+		got := checkArea(rectangle) // use the checkArea function to calculate the area of the rectangle
 		want := 72.0
 		if got != want {
 			t.Errorf("got %.2f, want %.2f", got, want)
@@ -23,14 +23,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("circle", func(t *testing.T) {
 		circle := Circle{10}
-		// got := Area(circle) --> this will throw error
-		// since we cannot use circle (variable of type Circle) as Rectangle value in argument to Area
-		// also, we can't declare another Area function with Circle as argument.
-		// so, we have only two options:
-		// 1. define another Area function with Circle as argument in a different package
-		// 2. use methods (Receiver functions) to define Area function with Circle as argument.
-		// we will use the second option.
-		got := circle.Area()
+		got := checkArea(circle) // use the checkArea function to calculate the area of the circle
 		want := 314.1592653589793
 		if got != want {
 			t.Errorf("got %g, want %g", got, want)
