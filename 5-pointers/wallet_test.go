27edb8d359ab04a9464bffbb7e295b66c649b5c8
viveksingh-01 <viveksingh9301@gmail.com>
2025-01-14 00:47:26 +0530

    Refactor Wallet to use Bitcoin type for balance
    update Deposit and Balance methods accordingly

--- 5-pointers/wallet_test.go
+++ 5-pointers/wallet_test.go
@@ -5,21 +5,11 @@ import "testing"
 func TestWallet(t *testing.T) {
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{balance: 0}
-		wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 		got := wallet.Balance()
-		want := 10
+		want := Bitcoin(10)
 		if got != want {
 			t.Errorf("got %d want %d", got, want)
 		}
 	})
-
-// NEED FOR POINTERS
-// When we run the test, it fails with the following error:
-// got 0 want 10
-// This is because the Deposit method does not actually modify the balance field of the Wallet struct.
-// This is because Go copies the Wallet when we call Deposit, so the method is actually working on a 
-// copy of the original Wallet.
-// To fix this, we need to change the Deposit method to use a pointer receiver.
-// This means that the method will be working with a pointer to the original Wallet, rather than a copy.
-// We can do this by changing the receiver for the Deposit method from (w Wallet) to (w *Wallet).
 }
