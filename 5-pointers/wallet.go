f4d1872b53a2fff5ae63a91a45e629eb2b7e986b
viveksingh-01 <viveksingh9301@gmail.com>
2025-01-14 01:01:15 +0530

    Add Withdraw method to Wallet and corresponding test case

--- 5-pointers/wallet.go
+++ 5-pointers/wallet.go
@@ -8,19 +8,16 @@ type Wallet struct {
 	balance Bitcoin
 }
 
-// Stringer interface
-// type Stringer interface {
-// 		String() string
-// }
-// A Stringer is a type that can describe itself as a string. 
-// The fmt package (and many others) look for this interface to print values.
-// The String method will be used to print values passed to %s in format strings.
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- 5-pointers/wallet_test.go
+++ 5-pointers/wallet_test.go
@@ -12,4 +12,14 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
