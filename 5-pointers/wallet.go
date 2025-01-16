abb3177e2b7055647e75a52581f92652429a8b37
Vivek Singh <viveksingh9301@gmail.com>
2025-01-16 21:50:13 +0530

    Add error handling to Withdraw method and enhance tests for insufficient funds

--- 5-pointers/wallet.go
+++ 5-pointers/wallet.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -16,8 +19,12 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if (amount > w.balance) {
+		return errors.New("insufficient funds, can't withdraw")
+	}
 	w.balance -= amount
+	return nil
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- 5-pointers/wallet_test.go
+++ 5-pointers/wallet_test.go
@@ -3,14 +3,6 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
-		t.Helper()
-		got := wallet.Balance()
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{balance: 0}
 		wallet.Deposit(Bitcoin(10))
@@ -22,4 +14,22 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{balance: startingBalance}
+		err := wallet.Withdraw(Bitcoin(50))
+		if err == nil {
+			t.Error("Wanted an error but didn't get one.")
+		}
+		assertBalance(t, wallet, startingBalance)
+	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
 }
