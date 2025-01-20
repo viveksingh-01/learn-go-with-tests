6f8c6a73bdd4cd8d997868f1480f4e5d67fac8b2
Vivek Singh <viveksingh9301@gmail.com>
2025-01-20 20:51:08 +0530

    Refactor dictionary package to use Dictionary type and update Search method

--- 6-maps/dictionary_test.go
+++ 6-maps/dictionary_test.go
@@ -3,10 +3,8 @@ package dictionary
 import "testing"
 
 func TestSearch(t *testing.T) {
-	// Maps are unordered collections of key-value pairs.
-	// They are declared using the map keyword, the key type, and the value type.
-	dictionary := map[string]string{"test": "this is just a test"}
-	got := Search(dictionary, "test")
+	dictionary := Dictionary{"test": "this is just a test"}
+	got := dictionary.Search("test")
 	want := "this is just a test"
 	if got != want {
 		t.Errorf("got %q want %q, given %q", got, want, "test")
