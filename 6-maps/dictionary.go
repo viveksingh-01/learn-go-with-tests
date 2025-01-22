806f76d6de6aae786e24c8e248a9faf7d2be3c30
Vivek Singh <viveksingh9301@gmail.com>
2025-01-22 09:31:12 +0530

    Update Dictionary Search method to return error for unknown words and enhance test coverage

--- 6-maps/dictionary.go
+++ 6-maps/dictionary.go
@@ -1,7 +1,17 @@
 package dictionary
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	// Here, we are using an interesting property of the map lookup.
+	// It can return 2 values. The second value is a boolean which indicates if the key was found successfully.
+	// This property allows us to differentiate between a word that doesn't exist and a word that
+	// just doesn't have a definition.
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return definition, nil
 }
