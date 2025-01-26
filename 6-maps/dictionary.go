6821dbf6ba9439bb5f19ff5095e45882f0110833
Vivek Singh <viveksingh9301@gmail.com>
2025-01-27 00:35:58 +0530

    Refactor error handling in Dictionary by introducing custom error types

--- 6-maps/dictionary.go
+++ 6-maps/dictionary.go
@@ -1,12 +1,18 @@
 package dictionary
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
-var ErrWordExists = errors.New("cannot add word because it already exists")
-var ErrWordDoesNotExist = errors.New("cannot perform operation on word because it does not exist")
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
