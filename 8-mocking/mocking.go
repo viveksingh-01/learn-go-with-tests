42f254248b4d5bea03890ce431c6755932f784ea
Vivek Singh <viveksingh9301@gmail.com>
2025-02-01 23:29:29 +0530

    Refactor Countdown function to use constants for countdown values

--- 8-mocking/mocking.go
+++ 8-mocking/mocking.go
@@ -6,17 +6,19 @@ import (
 	"os"
 )
 
+const countdownStart = 3
+const finalWord = "Go!"
+
 // Countdown function to count from 3 to 1
 // We know we want our Countdown function to write data somewhere and io.Writer is
 // the de-facto way of capturing that as an interface in Go.
 // In main we will send to os.Stdout so our users see the countdown printed to the terminal.
 // In test we will send to bytes.Buffer so our tests can capture what data is being generated.
-
 func Countdown(out io.Writer) {
-	for i := 3; i >= 1; i-- {
+	for i := countdownStart; i >= 1; i-- {
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
