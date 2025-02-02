60bb06ae4d54ea69b194f59b02ea778982cff078
Vivek Singh <viveksingh9301@gmail.com>
2025-02-02 09:42:26 +0530

    Implement dependency injection for Countdown function with a Sleeper interface and add tests for Sleep calls

--- 8-mocking/mocking.go
+++ 8-mocking/mocking.go
@@ -4,23 +4,49 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const countdownStart = 3
 const finalWord = "Go!"
 
-// Countdown function to count from 3 to 1
-// We know we want our Countdown function to write data somewhere and io.Writer is
-// the de-facto way of capturing that as an interface in Go.
-// In main we will send to os.Stdout so our users see the countdown printed to the terminal.
-// In test we will send to bytes.Buffer so our tests can capture what data is being generated.
-func Countdown(out io.Writer) {
+// If we can mock 'time.Sleep' we can use dependency injection to use it instead of
+// a "real" time.Sleep and then we can spy on the calls to make assertions on them.
+
+// Here, we are creating an interface for Sleeper and then implementing it with
+// DefaultSleeper and SpySleeper. This way, we can use the SpySleeper to spy on
+// the calls to Sleep() and make assertions on them.
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+type SpySleeper struct {
+	Calls int
+}
+
+// DefaultSleeper implements the Sleeper interface and sleeps for 1 second.
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+// Spies are a kind of mock which can record how a dependency is used.
+// They can record the arguments sent in, how many times it has been called, etc.
+// In our case, we're keeping track of how many times Sleep() is called so we can check it in our test.
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i >= 1; i-- {
 		fmt.Fprintln(out, i)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- 8-mocking/mocking_test.go
+++ 8-mocking/mocking_test.go
@@ -10,12 +10,19 @@ const expectedCountdown = `3
 1
 Go!`
 
+// If we can mock time.Sleep we can use dependency injection to use it instead of
+// a "real" time.Sleep and then we can spy on the calls to make assertions on them.
 func TestCountdown(t *testing.T) {
-	buffer := bytes.Buffer{}
-	Countdown(&buffer)
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+	Countdown(buffer, spySleeper)
 	got := buffer.String()
 	want := expectedCountdown
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 	}
 }
