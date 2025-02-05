020ca395e1edc98bb904ccdec176bd9c8cbb8a1e
Vivek Singh <viveksingh9301@gmail.com>
2025-02-05 22:01:27 +0530

    Add benchmark test for CheckWebsites with slowStubWebsiteChecker

--- 9-concurrency/check_website_test.go
+++ 9-concurrency/check_website_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -24,4 +25,25 @@ func TestCheckWebsites(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("wanted %v, got %v", want, got)
 	}
+}
+
+// The benchmark tests CheckWebsites using a slice of one hundred urls and uses
+// a new fake implementation of WebsiteChecker - slowStubWebsiteChecker
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	// We use b.ResetTimer() in this test to reset the time of our test before it actually runs
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
+
+// slowStubWebsiteChecker is deliberately slow.
+// It uses time.Sleep to wait exactly 20 milliseconds and then it returns true.
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
 }
