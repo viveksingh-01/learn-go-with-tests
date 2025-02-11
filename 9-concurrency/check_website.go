97e6cead5c114be06f6a7f7f31423fcf9b071ec6
Vivek Singh <viveksingh9301@gmail.com>
2025-02-11 22:24:42 +0530

    Channels in Go

--- 9-concurrency/check_website.go
+++ 9-concurrency/check_website.go
@@ -2,31 +2,40 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
-// On running the benchmark test with our newly implemented Goroutine, we get error -
-// fatal error: concurrent map writes.
-// Sometimes, when we run our tests, two of the goroutines write to the results map
-// at exactly the same time. Maps in Go don't like it when more than one thing tries
-// to write to them at once, and so fatal error.
-
-// This is a race condition, a bug that occurs when the output of our software is dependent
-// on the timing and sequence of events that we have no control over.
-// Because we cannot control exactly when each goroutine writes to the results map, we are
-// vulnerable to two goroutines writing to it at the same time.
-// We can solve this data race by coordinating our goroutines using CHANNELS.
+// The new type, result has been made to associate the return value of the
+// WebsiteChecker with the url being checked - it's a struct of string and bool.
+// As we don't need either value to be named, each of them is anonymous within the struct;
+// this can be useful in when it's hard to know what to name a value.
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	// We create our 'resultChannel' channel, using make in the same way as map.
+	// chan result is the type of the channel - a channel of result.
+	resultChannel := make(chan result)
+	// Now when we iterate over the urls, instead of writing to the map directly we're sending a
+	// result struct for each call to wc to the resultChannel with a send statement.
+	// This uses the <- operator, taking a channel on the left and a value on the right:
 	for _, url := range urls {
 		go func() {
-			results[url] = wc(url)
+			// By sending the results into a channel, we can control the timing of each write into the
+			// results map, ensuring that it happens one at a time.
+			resultChannel <- result{url, wc(url)}
+			// Although each of the calls of wc, and each send to the result channel, is happening
+			// concurrently inside its own process, each of the results is being dealt with one at a
+			// time as we take values out of the result channel with the receive expression.
 		}()
+	}
+	// The next for loop iterates once for each of the urls.
+	// Inside we're using a receive expression, which assigns a value received from a channel to a variable.
+	// This also uses the <- operator, but with the two operands now reversed:
+	// the channel is now on the right and the variable that we're assigning to is on the left:
+	for range urls {
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 	return results
 }
-
-// CHANNELS in Go:
-// Channels are a Go data structure that can both receive and send values.
-// These operations, along with their details, allow communication between different processes.
-
-// In this case we want to think about the communication between the parent process and each of
-// the goroutines that it makes to do the work of running the WebsiteChecker function with the url.
