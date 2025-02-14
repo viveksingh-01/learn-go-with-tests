052ccd3f41c6be771927a16dba9cd719bc32a979
Vivek Singh <viveksingh9301@gmail.com>
2025-02-14 21:56:34 +0530

    Refactor Racer to use a separate function for measuring response time

--- 10-select/racer.go
+++ 10-select/racer.go
@@ -6,20 +6,17 @@ import (
 )
 
 func Racer(a, b string) (winner string) {
-	// We use 'Now()' method on 'time' to record just before we try and get the URL
-	startA := time.Now()
-	// Then we use http.Get to try and perform an HTTP GET request against the URL.
-	// This function returns an http.Response and an error
-	http.Get(a)
-	// time.Since takes the start time and returns a time.Duration of the difference
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
 }
