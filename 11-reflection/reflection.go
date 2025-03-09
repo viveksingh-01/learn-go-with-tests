2a4266c1a7b72e96e3b434ecc3cb1c1f70755f84
Vivek Singh <viveksingh9301@gmail.com>
2025-03-09 10:14:42 +0530

    Update Walk function to filter non-string fields and enhance test coverage

--- 11-reflection/reflection.go
+++ 11-reflection/reflection.go
@@ -17,10 +17,13 @@ func Walk(x interface{}, fn func(input string)) {
 
 	// val has a method NumField which returns the number of fields in the value.
 	// This lets us iterate over the fields and call fn which passes our test.
-	for i := 0; i < val.NumField(); i++ {
+	for i := range val.NumField() {
 		// val.Field(i) returns the i-th field of the value.
 		// We can then call String() on the field to get the string value.
 		field := val.Field(i)
-		fn(field.String())
+		// Kind helps us to check the type of the field
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
 	}
 }
