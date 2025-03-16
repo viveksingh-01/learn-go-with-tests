0273bfdba1798d59e668d3df9e5e6f4a41ca081c
Vivek Singh <viveksingh9301@gmail.com>
2025-03-16 15:54:47 +0530

    Enhance Walk function to support slices and update tests for improved coverage

--- 11-reflection/reflection.go
+++ 11-reflection/reflection.go
@@ -5,19 +5,19 @@ import "reflect"
 func Walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	// val has a method NumField which returns the number of fields in the value.
-	// This lets us iterate over the fields and call fn which passes our test.
-	for i := range val.NumField() {
-		// val.Field(i) returns the i-th field of the value.
-		// We can then call String() on the field to get the string value.
-		field := val.Field(i)
-		// Kind helps us to check the type of the field
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			Walk(field.Interface(), fn)
+	switch val.Kind() {
+	case reflect.Struct:
+		// NumField which returns the number of fields in the value.
+		for i := range val.NumField() {
+			Walk(val.Field(i).Interface(), fn)
 		}
+	case reflect.Slice:
+		// Len returns the number of elements in the slice.
+		for i := range val.Len() {
+			Walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
 	}
 }
 
